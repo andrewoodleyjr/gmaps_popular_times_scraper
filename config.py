7d5fa29c9861fc93143957e7a44096d3f5111dd9
Fabricio Murai <fabriciomurai@gmail.com>
2024-11-26 21:11:50 -0500

    - Minor changes

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ HEADER_COLUMNS = ('place', 'url', 'scrape_time', 'day_of_week', 'hour_of_day', '
 
 # path to chrome and chromedriver
 CHROME_BINARY_LOCATION = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-CHROMEDRIVER_BINARY_LOCATION = '/usr/local/bin/chromedriver'
+# 11/26/2024: Selenium doesn't have that option anymore; use EXPORT
+# CHROMEDRIVER_BINARY_LOCATION = '/usr/local/bin/chromedriver'
 
 # keep an cache of the source htmls, with a timestamp in the filename
 # if so, they should be cleaned out once in a while, since they are 1MB each
@@ -17,8 +18,8 @@ SAVE_HTML = False
 
 # put your url or path here to a csv where the first column is a google maps url
 # google sheets - export as csv https://stackoverflow.com/a/33727897/2327328
-URL_PATH_INPUT = None
-#URL_PATH_INPUT = 'urls.csv'
+# URL_PATH_INPUT = None
+URL_PATH_INPUT = 'urls.csv'
 
 DEBUG = False
 URL_PATH_INPUT_TEST = 'tests' + os.sep + 'test_urls.csv'
--- scrape_gm.py
+++ scrape_gm.py
@@ -43,8 +43,14 @@ def main():
 	# write to folder logs to remember the state of the config file
 	urls.to_csv('logs' + os.sep + run_time + '.log', index = False)
 
-	url_list = urls.iloc[:, 0].tolist()
-	for url in url_list:
+	# url_list = urls.iloc[:, 0].tolist()
+	# for url in url_list:
+	for index, row in urls.iterrows():
+		url = row['#url']
+		if len(row) > 1:
+			name = row['name']
+		else:
+			name = url
 		#print(urllib.parse.urlparse(url))
 		#print (url)
 
@@ -65,7 +71,8 @@ def main():
 
 				# write data
 				for row in data:
-					f.write(config.DELIM.join((file_name,url,run_time)) + config.DELIM + config.DELIM.join([str(x or '') for x in row])+'\n')
+					# f.write(config.DELIM.join((file_name,url,run_time)) + config.DELIM + config.DELIM.join([str(x or '') for x in row])+'\n')
+					f.write(config.DELIM.join((name,url,run_time)) + config.DELIM + config.DELIM.join([str(x or '') for x in row])+'\n')
 
 			print('DONE:', url, run_time)
 
@@ -118,10 +125,25 @@ def get_html(u,file_name):
 		options.add_argument('--headless')
 		# https://stackoverflow.com/a/55152213/2327328
 		# I choose German because the time is 24h, less to parse
-		options.add_argument('--lang=de-DE')
+		# options.add_argument('--lang=de-DE')
+		options.add_argument('--lang=en-US')
+		prefs = {
+			# block image loading
+			"profile.managed_default_content_settings.images": 2,
+		}
+
+		# 11/26/2024: It may be worth checking if these options make it faster
+		# options.add_argument("--headless=new")
+		# options.add_argument("--disable-extensions")
+	
+		options.add_experimental_option("prefs", prefs)
 		options.binary_location = config.CHROME_BINARY_LOCATION
-		chrome_driver_binary = config.CHROMEDRIVER_BINARY_LOCATION
-		d = webdriver.Chrome(chrome_driver_binary, options=options)
+		
+		# 11/26/2024: chrome_driver_binary location can no longer be passed
+		# chrome_driver_binary = config.CHROMEDRIVER_BINARY_LOCATION
+		
+		# d = webdriver.Chrome(chrome_driver_binary, options=options)
+		d = webdriver.Chrome(options=options)
 
 		# get page
 		d.get(u)
@@ -130,7 +152,11 @@ def get_html(u,file_name):
 		# timeout after max N seconds (config.py)
 		# based on https://stackoverflow.com/questions/26566799/wait-until-page-is-loaded-with-selenium-webdriver-for-python
 		try:
-			WebDriverWait(d, config.SLEEP_SEC).until(EC.presence_of_element_located((By.CLASS_NAME, 'section-popular-times-bar')))
+			# wait until page is completed loaded
+
+			# wait until a tag that starts with 'Popular times at' is present
+			WebDriverWait(d, config.SLEEP_SEC).until(EC.presence_of_element_located((By.XPATH, '//*[starts-with(@id, "Popular times at")]')))
+			# WebDriverWait(d, config.SLEEP_SEC).until(EC.presence_of_element_located((By.CLASS, 'fMc7Ne mQXJne gjs6Ee')))
 		except TimeoutException:
 			print('ERROR: Timeout! (This could be due to missing "popular times" data, or not enough waiting.)',u)
 
@@ -149,52 +175,67 @@ def parse_html(html):
 
 	soup = BeautifulSoup(html,features='html.parser')
 
-	pops = soup.find_all('div', {'class': 'section-popular-times-bar'})
+	# there is no class 'section-popular-times-bar'
+	# pops = soup.find_all('div', {'class': 'section-popular-times-bar'})
+	# find element containing a tag that starts with 'Popular times at'
+	div_tag_list = soup.find(
+					lambda tag:tag.name == "div" and tag.has_attr('aria-label') and
+					str.startswith(tag["aria-label"],'Popular times at'))
 
-	hour = 0
-	dow = 0
+
+	# find div containing 7 divs (one for each week day):
+	for div_tag in div_tag_list:
+		if len(div_tag.find_all('div', recursive=False)) == 7:
+			break
+
+	# hour = 0
+	# dow = 0
 	data = []
-
-	for pop in pops:
-		# note that data is stored sunday first, regardless of the local
-		t = pop['aria-label']
-		# debugging
-		#print(t)
-
-		hour_prev = hour
+	for dow, weekday_tag in enumerate(div_tag):
+		pops = weekday_tag.find_all(lambda tag:tag.name == "div" and tag.has_attr('aria-label'))
+		hour = -1
 		freq_now = None
 
-		try:
-			if 'normal' not in t:
-				hour = int(t.split()[1])
-				freq = int(t.split()[4]) # gm uses int
-			else:
-				# the current hour has special text
-				# hour is the previous value + 1
-				hour = hour + 1
-				freq = int(t.split()[-2])
+		# iterate over hours to get popularity data
+		for pop in pops:
+			# note that data is stored sunday first, regardless of the local
+			t = pop['aria-label']
+			# debugging
+			# print(t)
 
-				# gmaps gives the current popularity,
-				# but only the current hour has it
-				try:
-					freq_now = int(t.split()[2])
-				except:
-					freq_now = None
+			try:
+				# if the text doesn't contain 'usually', it's a regular hour
+				if 'usually' not in t:
+					hour = int(t.split()[3])
+					freq = int(t.split('%')[0]) # gm uses int
+				else:
+					# the current hour has special text
+					# hour is the previous value + 1
+					hour = hour + 1
+					freq = int((t.split()[-2]).split('%')[0])
 
-			if hour < hour_prev:
-				# increment the day if the hour decreases
+					# gmaps gives the current popularity,
+					# but only the current hour has it
+					try:
+						freq_now = int((t.split()[1]).split('%')[0])
+					except:
+						freq_now = None
+
+				# if hour < hour_prev:
+				# 	# increment the day if the hour decreases
+				# 	dow += 1
+
+				data.append([days[dow], hour, freq, freq_now])
+				freq_now = None
+				# could also store an array of dictionaries
+				#data.append({'day' : days[dow % 7], 'hour' : hour, 'popularity' : freq})
+
+			except:
+				# if a day is missing, the line(s) won't be parsable
+				# this can happen if the place is closed on that day
+				# skip them, hope it's only 1 day per line,
+				# and increment the day counter
 				dow += 1
-
-			data.append([days[dow % 7], hour, freq, freq_now])
-			# could also store an array of dictionaries
-			#data.append({'day' : days[dow % 7], 'hour' : hour, 'popularity' : freq})
-
-		except:
-			# if a day is missing, the line(s) won't be parsable
-			# this can happen if the place is closed on that day
-			# skip them, hope it's only 1 day per line,
-			# and increment the day counter
-			dow += 1
 
 	return data
 
