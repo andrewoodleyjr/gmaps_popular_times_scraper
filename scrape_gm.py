ffab3c62ced15210b195c0a907f8a651220fefe9
André Woodley Jr. <hello@andrewoodleyjr.com>
2025-01-09 07:20:29 -0500

    import options + service (saved)

--- scrape_gm.py
+++ scrape_gm.py
@@ -13,6 +13,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from datetime import datetime
 import pandas as pd
